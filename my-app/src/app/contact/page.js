b634b7be463f1320b7efffd23ca839ef1dba5a43
dolfik1417 <bosun4hip@gmail.com>
2024-10-14 14:56:41 +0100

    your msg

---
+++ my-app/src/app/contact/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function conatc() {
+  return (
+    <div className='bg-blue-500'>
+      Contact
+    </div>
+  )
+}
---
+++ my-app/src/components/useContext.js
@@ -0,0 +1,36 @@
+import {useContext} from 'react';
+import {useState} from 'react';
+
+const ThemeContext = React.createContext();
+
+function App() {
+    const [theme, setTheme] = useState("light");
+
+    return (
+        <ThemeContext.Provider value={{ theme, setTheme}}>
+            <toolbar />
+        </ThemeContext.Provider>
+    );
+}
+
+function Toolbar() {
+    return (
+        <div>
+            <ThemeButton />
+        </div>
+    );
+}
+
+function ThemeButton() {
+    const { theme, setTheme } = useContext(ThemeContext);
+
+    return (
+        <button onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+            style={{background: theme === "light" ? "#fff" : "#333", color: theme === "light" ? "#000" : "#fff" }}
+        >
+            Toggle Theme
+        </button>
+    );
+}
+
+export default useContext
